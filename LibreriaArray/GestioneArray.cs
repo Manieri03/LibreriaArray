0e81cb9d600b9d98a842ed76aee5b7ff1d780f90
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-13 13:00:55 +0100

    Aggiunti metodi leggi e stampa

---
+++ LibreriaArray/GestioneArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LibreriaArray
+{
+    public class GestioneArray
+    {
+        public static void LeggiArray(int[] array)
+        {
+            for(int i =0;i<array.Length; i++)
+            {
+                Console.WriteLine($"Inserisci l numero in posizione {i}");
+                int a = int.Parse(Console.ReadLine());
+                array[i] = a;
+            }
+
+        }
+        public static void StampaArray(int[] array)
+        {
+            Console.WriteLine("I valori sono: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+" ");
+            }
+
+        }
+
+    }
+}
---
+++ Prova/Program.cs
@@ -0,0 +1,17 @@
+﻿using LibreriaArray;
+using System;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            GestioneArray.LeggiArray(array);
+            GestioneArray.StampaArray(array);
+            Console.ReadLine();
+            
+        }
+    }
+}
